7826e580d659e09d750fa38e9c4b455810408261
Jatin Joshi <jatinjoshi905@gmail.com>
2025-01-07 12:33:02 -0800

    header work

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Footer from "@/components/common/Footer";
 import RequestCall from "@/components/homepage/RequestCall";
+import Header from "@/components/common/Header";
 
 export default function Home() {
   return (
     <>
+      <Header />
       <RequestCall />
       <Footer />
     </>
