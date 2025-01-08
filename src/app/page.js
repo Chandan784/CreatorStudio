8babb22be7560decb35ce3f8704b38786f978609
Jatin Joshi <jatinjoshi905@gmail.com>
2025-01-08 00:32:28 -0800

    Steps section

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Footer from "@/components/common/Footer";
 import RequestCall from "@/components/homepage/RequestCall";
 import Header from "@/components/common/Header";
 import Details from "@/components/common/Details";
+import StepsToPartner from "@/components/homepage/StepsToPartner";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Details />
+      <StepsToPartner />
       <RequestCall />
       <Footer />
     </>
