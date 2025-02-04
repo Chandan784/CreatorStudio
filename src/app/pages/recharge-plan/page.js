337ee9beae8d2266005ea9ed7f754d702935cc3c
TKBiswalport <biswalt662@gmail.com>
2025-02-05 02:19:30 +0530

    Recharge Plan all done

---
+++ src/app/pages/recharge-plan/page.js
@@ -0,0 +1,12 @@
+import Recharge from '@/components/recharge/Recharge'
+import RecomendedPlans from '@/components/recharge/RecomendedPlans'
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+        <Recharge/>
+        <RecomendedPlans/>
+    </>
+  )
+}
