a574422012602b0080832905a1ac78e934923961
biswaranjans7978 <biswaranjans7978@gmail.com>
2025-02-12 00:33:19 +0530

    addequipment page done

--- equipment-app/src/app/components/CategoryCard.js
+++ equipment-app/src/app/components/CategoryCard.js
@@ -1,21 +1,31 @@
+"use client";
 import { icons } from "lucide-react";
 import React from "react";
 import { FaCamera } from "react-icons/fa";
 import { FaLightbulb } from "react-icons/fa";
 import { FaMicrophone } from "react-icons/fa";
 import { TbAccessible } from "react-icons/tb";
+import { usePathname, useRouter } from "next/navigation";
+
+const CategoryCard =()=> {
+    const router= useRouter();
+    const pathname=usePathname();
 const categories = [
-    { name: "Camera", icon: <FaCamera className="text-sky-400"/> },
-    { name: "Lights", icon: <FaLightbulb className="text-pink-500"/> },
-    { name: "Microphone", icon: <FaMicrophone className="text-green-500 ml-3"/> },
-    { name: "Accessories", icon: <TbAccessible className=" text-red-600 ml-4"/>},
+    { name: "Camera", icon: <FaCamera className="text-sky-400"/>,route:"./addequipment" },
+    { name: "Lights", icon: <FaLightbulb className="text-pink-500"/>,route:"/addequipment"},
+    { name: "Microphone", icon: <FaMicrophone className="text-green-500 ml-3"/>,route:"/addequipment" },
+    { name: "Accessories", icon: <TbAccessible className=" text-red-600 ml-4"/>,route:"./addequipment"  },
   ];
 
-  export default function CategoryCard() {
+  
     return (
       <div className="flex justify-around my-4">
         {categories.map((cat) => (
-          <div key={cat.name} className="text-center bg-slate-200 border rounded-md p-2">
+          <div key={cat.name} 
+          onClick={()=>router.push(cat.route)}
+          className={`text-center bg-slate-200 border rounded-md p-2 ${
+            pathname === cat.route ? "text-blue-500" : "text-gray-500"
+          }`}>
             <div className="text-3xl pl-2">{cat.icon}</div>
             <p>{cat.name}</p>
           </div>
@@ -23,3 +33,5 @@ const categories = [
       </div>
     );
   }
+
+  export default CategoryCard;
