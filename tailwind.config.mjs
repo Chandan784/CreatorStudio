01cae10168182e8459e56aa60eed42cd33f54a18
chandan <chandansamantaray784@gmail.com>
2025-03-11 17:46:33 +0530

    changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,25 +7,9 @@ export default {
   ],
   theme: {
     extend: {
-<<<<<<< HEAD
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      animation: {
-        blink: "blink 1.5s infinite",
-      },
-      keyframes: {
-        blink: {
-          "0%, 100%": { opacity: 1 },
-          "50%": { opacity: 0.4 },
-        },
-      },
-=======
       backgroundImage: {
-        check: "url('/assets/images/home/webp/people-check-in.webp')"
-      }
->>>>>>> a288be371c1cd60fe305e0f6ba79582dc95e9692
+        check: "url('/assets/images/home/webp/people-check-in.webp')",
+      },
     },
   },
   plugins: [],
