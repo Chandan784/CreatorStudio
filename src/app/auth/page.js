3f48c7321900b32645faadb3013ab4ee4778657e
TKBiswalport <biswalt662@gmail.com>
2025-03-20 18:30:14 +0530

    Rendering Message on Signin Page

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -198,6 +198,7 @@ export default function Authentication() {
                 handleAuth={handleAuth}
                 setIsLogin={setIsLogin}
                 setMessage={setMessage}
+                message= {message}
               />
             )
           ) : (
